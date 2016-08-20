5b6386ba30468a5fe9b035c81fe803a605727ac4
Jonathan Speek <jonspeek@gmail.com>
2016-08-20 15:26:54 -0600

    Added auth model, views, and controller

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,4 +1,5 @@
 class NotesController < ApplicationController
+  before_action :find_note, only: [:show, :edit, :update, :destroy]
 
   def index
   end
@@ -7,25 +8,36 @@ class NotesController < ApplicationController
   end
 
   def new
+    @note = Note.new
   end
 
   def create
+    @note = Note.new(note_params)
   end
 
   def edit
   end
 
   def update
+    if @note.update(note_params)
+      redirect_to @note
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @note.destroy
+    redirect_to notes_path
   end
 
   private
 
   def find_note
+    @note = Note.find(params[:id])
   end
 
   def note_params
+    params.require(:note).permit(:title, :content)
   end
 end
