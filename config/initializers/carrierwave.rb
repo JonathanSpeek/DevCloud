b3f929d4a1a1d93cd559069edd9aa18f2b3e3915
Jonathan Speek <jonspeek@gmail.com>
2016-08-21 10:55:26 -0600

    Changed region

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,7 @@ CarrierWave.configure do |config|
       provider: 'AWS',
       aws_access_key_id:'AKIAIG7OKTNMNO5OJJDQ',
       aws_secret_access_key: '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',
-      region: 'Oregon',
+      region: 'us-west-2',
   }
   config.fog_directory = 'speekdevcloud'
 end
