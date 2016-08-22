ae3294ebdd99d9b616ed1dd4da5d89c01e20dbb0
Jonathan Speek <jonspeek@gmail.com>
2016-08-21 19:44:04 -0600

    styled index notes

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,6 +5,6 @@ CarrierWave.configure do |config|
       :aws_secret_access_key => ENV['AWS_SECRET'],
       region: 'us-west-2',
   }
-  config.fog_directory = 'speekdevcloud'herok
+  config.fog_directory = 'speekdevcloud'
 end
 
