e032037c53f56d5dcfccb8fe82a7f01d04380159
Jonathan Speek <jonspeek@gmail.com>
2016-08-24 07:40:37 -0600

    Added SendGrid for mailer on forgot pass

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,8 +11,28 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
+
+  config.action_mailer.default_url_options = { :host => 'devcloud1.herokuapp.com' }
+
+  Rails.application.routes.default_url_options[:host] = 'devcloud1.herokuapp.com'
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+
+  ActionMailer::Base.smtp_settings = {
+      :address        => 'smtp.sendgrid.net',
+      :port           => '587',
+      :authentication => :plain,
+      :user_name      => ENV['SENDGRID_USERNAME'],
+      :password       => ENV['SENDGRID_PASSWORD'],
+      :domain         => 'heroku.com',
+      :enable_starttls_auto => true
+  }
+
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
