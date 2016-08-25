c81412d634aded70e14cc0bd03a9300f2bf80106
Jonathan Speek <jonspeek@gmail.com>
2016-08-25 06:26:01 -0600

    Trying to fix Github callback

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks' }
+  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
   get 'welcome/index'
   resources :notes
   resources :uploads
