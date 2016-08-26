5bbd84b1f1ca69c5a708c92f6a48219311368d1a
Jonathan Speek <jonspeek@gmail.com>
2016-08-25 19:48:51 -0600

    Revert "Revert "Added logic to handle duplicate email on multiple providers""
    This reverts commit 6079dba0cee3e90d532c9775640445971eaed42e.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,21 @@ class User < ActiveRecord::Base
   has_many :uploads
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.provider = auth.provider
-      user.uid = auth.uid
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
+    if self.where(email: auth.info.email).exists?
+      return_user = self.where(email: email).first
+      return_user.provider = auth.provider
+      return_user.uid = auth.uid
+    else
+      return_user = self.create do |user|
+        user.provider = auth.provider
+        user.uid = auth.uid
+        user.name = auth.info.name
+        user.username = auth.info.username
+        user.email = auth.info.email
+        user.oauth_token = auth.credentials.token
+        user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      end
     end
   end
 end
+
