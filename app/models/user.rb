608734a8520bdf2bccf04d1c03b9d492c867a8f0
Jonathan Speek <jonspeek@gmail.com>
2016-08-25 19:36:43 -0600

    Revert "Adddressing _oauth2 error"
    This reverts commit 7ca3271d47c7cd9a0afae13456ac8133299dc46f.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:github, :slack, :google_oauth2]
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:github, :slack, :google]
   has_many :notes
   has_many :uploads
 
