f43a8f8250e2b097b2b9b30f9436b766787ae73e
Jonathan Speek <jonspeek@gmail.com>
2016-08-26 18:01:01 -0600

    Added info and image to callbacks_controller and image trial to view

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,6 +1,6 @@
 class CallbacksController < Devise::OmniauthCallbacksController
   def github
-    @user = User.from_omniauth(request.env["omniauth.auth"])
+    @user = User.from_omniauth(request.env["omniauth.auth"]["info"]["image"])
     sign_in_and_redirect @user
   end
   def slack
