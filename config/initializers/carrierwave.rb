cc69a1da21ac6b7b67f085ab3f9097cb975e1809
jonathanspeek <jonspeek@gmail.com>
2016-10-05 12:20:12 -0600

    precompiled assets, changed path to svg, commented out carrierwave.rb in initializers

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,10 @@
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-      provider: 'AWS',
-      :aws_access_key_id => ENV['AWS_KEY'],
-      :aws_secret_access_key => ENV['AWS_SECRET'],
-      region: 'us-west-2',
-  }
-  config.fog_directory = 'speekdevcloud'
-end
+# CarrierWave.configure do |config|
+#   config.fog_credentials = {
+#       provider: 'AWS',
+#       :aws_access_key_id => ENV['AWS_KEY'],
+#       :aws_secret_access_key => ENV['AWS_SECRET'],
+#       region: 'us-west-2',
+#   }
+#   config.fog_directory = 'speekdevcloud'
+# end
 
