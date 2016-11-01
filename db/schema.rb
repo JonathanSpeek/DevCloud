48d1d55cc08202e6f3fd5c6ad0ddcc63dae0a759
jonathanspeek <jonspeek@gmail.com>
2016-11-01 09:08:19 -0600

    Address Rubocop offenses that I care about

--- db/schema.rb
+++ db/schema.rb
@@ -12,61 +12,72 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20160826234144) do
-
-  create_table "identities", force: :cascade do |t|
-    t.integer  "user_id"
-    t.string   "provider"
-    t.string   "uid"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  # Creates identity for oauth users
+  create_table 'identities', force: :cascade do |t|
+    t.integer  'user_id'
+    t.string   'provider'
+    t.string   'uid'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  add_index "identities", ["user_id"], name: "index_identities_on_user_id"
+  # Indexing user_id
+  add_index 'identities', ['user_id'], name: 'index_identities_on_user_id'
 
-  create_table "notes", force: :cascade do |t|
-    t.string   "title"
-    t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer  "user_id"
-    t.datetime "deleted_at"
+  # Notes/Wikis table
+  create_table 'notes', force: :cascade do |t|
+    t.string   'title'
+    t.text     'content'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.integer  'user_id'
+    t.datetime 'deleted_at'
   end
 
-  add_index "notes", ["deleted_at"], name: "index_notes_on_deleted_at"
+  # Indexing deleted_at
+  add_index 'notes', ['deleted_at'], name: 'index_notes_on_deleted_at'
 
-  create_table "uploads", force: :cascade do |t|
-    t.string   "name"
-    t.string   "attachment"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer  "user_id"
-    t.datetime "deleted_at"
+  # Uploads table for files
+  create_table 'uploads', force: :cascade do |t|
+    t.string   'name'
+    t.string   'attachment'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.integer  'user_id'
+    t.datetime 'deleted_at'
   end
 
-  add_index "uploads", ["deleted_at"], name: "index_uploads_on_deleted_at"
+  # Indexing deleted_at
+  add_index 'uploads', ['deleted_at'], name: 'index_uploads_on_deleted_at'
 
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.datetime "deleted_at"
-    t.string   "provider"
-    t.string   "uid"
-    t.string   "image"
-    t.string   "name"
+  # USers table
+  create_table 'users', force: :cascade do |t|
+    t.string   'email',                  default: '', null: false
+    t.string   'encrypted_password',     default: '', null: false
+    t.string   'reset_password_token'
+    t.datetime 'reset_password_sent_at'
+    t.datetime 'remember_created_at'
+    t.integer  'sign_in_count',          default: 0,  null: false
+    t.datetime 'current_sign_in_at'
+    t.datetime 'last_sign_in_at'
+    t.string   'current_sign_in_ip'
+    t.string   'last_sign_in_ip'
+    t.datetime 'created_at',                          null: false
+    t.datetime 'updated_at',                          null: false
+    t.datetime 'deleted_at'
+    t.string   'provider'
+    t.string   'uid'
+    t.string   'image'
+    t.string   'name'
   end
 
-  add_index "users", ["deleted_at"], name: "index_users_on_deleted_at"
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  # Indexing deleted_at
+  add_index 'users', ['deleted_at'], name: 'index_users_on_deleted_at'
+
+  # Indexing email
+  add_index 'users', ['email'], name: 'index_users_on_email', unique: true
+
+  # Indexing reset_password_token
+  add_index 'users', ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
 
 end
