3f8f018a38d5ab0fdca52fd9262f539d2af74db2
jonathanspeek <jonspeek@gmail.com>
2017-01-28 12:50:01 -0700

    Added subscription col to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160826234144) do
+ActiveRecord::Schema.define(version: 20170128194805) do
 
   create_table "identities", force: :cascade do |t|
     t.integer  "user_id"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 20160826234144) do
     t.string   "uid"
     t.string   "image"
     t.string   "name"
+    t.string   "subscription"
   end
 
   add_index "users", ["deleted_at"], name: "index_users_on_deleted_at"
