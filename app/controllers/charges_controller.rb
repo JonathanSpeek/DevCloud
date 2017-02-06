ad579d42720c39926a1b1fdb519d7bae6c412a42
jonathanspeek <jonspeek@gmail.com>
2017-02-06 16:39:49 -0700

    working on Stripe

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -7,19 +7,19 @@ class ChargesController < ApplicationController
     @amount = 999
 
     customer = Stripe::Customer.create(
-        :email => params[:stripeEmail],
-        :source  => params[:stripeToken]
+        email: params[:stripeEmail],
+        source: params[:stripeToken]
     )
 
     charge = Stripe::Charge.create(
-        :customer    => customer.id,
-        :amount      => @amount,
-        :description => 'Rails Stripe customer',
-        :currency    => 'usd'
+        customer: customer.id,
+        amount: @amount,
+        description: 'DevCloud monthly subscription',
+        currency: 'usd'
     )
 
   rescue Stripe::CardError => e
     flash[:error] = e.message
-    redirect_to new_charge_path
+    redirect_to root_path
   end
 end
