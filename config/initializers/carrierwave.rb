4fbf49d2100951cfe498245dc7be71b061ac5bb6
Jonathan Speek <jonspeek@gmail.com>
2017-07-19 16:07:14 -0600

    converted to haml, adjusted carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,13 @@
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: ENV['AWS_KEY'],
-    aws_secret_access_key: ENV['AWS_SECRET'],
-    region: 'us-west-2'
-  }
-  config.fog_directory = 'speekdevcloud'
+  if Rails.env.development? || Rails.env.test?
+    config.storage = :file
+  else
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['AWS_KEY'],
+      aws_secret_access_key: ENV['AWS_SECRET'],
+      region: 'us-west-2'
+    }
+    config.fog_directory = 'speekdevcloud'
+  end
 end
