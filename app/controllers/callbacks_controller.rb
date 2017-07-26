98046987caee1cddb77236fcc4597dd426fa334a
Jonathan Speek <jonspeek@gmail.com>
2017-07-26 03:36:55 -0600

    troubleshooting oauth google issues

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
-# Controller to handle Github redirect
+# Controller to handle OAuth redirect
 class CallbacksController < Devise::OmniauthCallbacksController
   def github
+    @user = User.from_omniauth(request.env['omniauth.auth'])
+    sign_in_and_redirect @user
+  end
+
+  def google_oauth2
     @user = User.from_omniauth(request.env['omniauth.auth'])
     sign_in_and_redirect @user
   end
