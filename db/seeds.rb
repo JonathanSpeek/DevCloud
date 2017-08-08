6ffce26c2c0953300858fd6d51ff7fe50636adea
Jonathan Speek <jonspeek@gmail.com>
2017-08-08 15:58:43 -0600

    Starting to add to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+if Rails.env.development?
+
+    # -------------------------------------------------------------------------------------------------------------------
+    # Test users
+    # -------------------------------------------------------------------------------------------------------------------
+    def create_user(email, password)
+      User.create_with(email: email, password: password).find_or_create_by(email: email)
+    end
+
+    puts 'Creating users'
+
+    create_user('alpha@example.com', 'alphapassword')
+    create_user('beta@example.com', 'betapassword')
+    create_user('gamma@example.com', 'gammapassword')
+
+    # -------------------------------------------------------------------------------------------------------------------
+    # Test notes
+    # -------------------------------------------------------------------------------------------------------------------
+    def create_note(title, content, user_id)
+      Note.create(title: title, content: content, user_id: user_id)
+    end
+
+    puts 'Creating notes'
+    alpha = User.where(email: 'alpha@example.com')[0].id
+    beta = User.where(email: 'beta@example.com')[0].id
+    gamma = User.where(email: 'gamma@example.com')[0].id
+
+    create_note('Ruby Test', "```ruby\r\ndef hello\r\n    puts 'hello'\r\nend\r\n```", alpha)
+    create_note('Ruby Test', "```ruby\r\ndef hello\r\n    puts 'hello'\r\nend\r\n```", beta)
+    create_note('Ruby Test', "```ruby\r\ndef hello\r\n    puts 'hello'\r\nend\r\n```", gamma)
+end
