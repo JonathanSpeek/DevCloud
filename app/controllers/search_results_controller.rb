c7d39d38def46dcf1f7ebe135bb4d020f64384ad
Jonathan Speek <jonspeek@gmail.com>
2017-10-27 12:23:23 -0600

    finally fixed search issues

--- app/controllers/search_results_controller.rb
+++ app/controllers/search_results_controller.rb
@@ -2,9 +2,12 @@
 # Controller to search results for notes, uploads, and folders
 class SearchResultsController < ApplicationController
   def index
+    @folders = Folder.where(user_id: current_user.id)
+    @notes = Note.where(user_id: current_user.id)
+    @uploads = Upload.where(user_id: current_user.id)
     search = params[:search]
-    @results = Folder.where('name LIKE ? ', "%#{search}%") +
-      Note.where('title LIKE ? OR content LIKE ? ', "%#{search}%", "%#{search}%") +
-      Upload.where('name LIKE ? ', "%#{search}%")
+    @results = @folders.where('name LIKE ? ', "%#{search}%") +
+      @notes.where('title LIKE ? OR content LIKE ? ', "%#{search}%", "%#{search}%") +
+      @uploads.where('name LIKE ? ', "%#{search}%")
   end
 end
